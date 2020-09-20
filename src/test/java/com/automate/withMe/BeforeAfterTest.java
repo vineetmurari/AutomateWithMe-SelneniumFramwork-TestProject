889b3a1332f21426a35224303e7b1a27131942b6
vineetmurari <68270056+vineetmurari@users.noreply.github.com>
2020-09-20 12:30:46 +0530

    Added Gecko Driver for FF browser
    Added Gecko Driver for FF browser

--- src/test/java/com/automate/withMe/BeforeAfterTest.java
+++ src/test/java/com/automate/withMe/BeforeAfterTest.java
@@ -2,6 +2,7 @@ package com.automate.withMe;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.testng.annotations.AfterTest;
@@ -27,6 +28,10 @@ public class BeforeAfterTest {
 			  System.setProperty("webdriver.ie.driver", "C:\\Users\\Dell\\Desktop\\Tutorials\\Drivers\\IEDriverServer.exe");
 				 driver= new InternetExplorerDriver();
 		  }
+		  else if(browser.equalsIgnoreCase("FF")){
+			  System.setProperty("webdriver.gecko.driver", "C:\\Users\\Dell\\Desktop\\Tutorials\\Drivers\\geckodriver.exe");
+				 driver= new FirefoxDriver();
+		  }
 		  else if(browser.equalsIgnoreCase("Headless")){
 			  WebDriver driver = new HtmlUnitDriver();
 		  }
@@ -34,8 +39,12 @@ public class BeforeAfterTest {
 
 	  @AfterTest
 	  public void afterTest() {
+		  try{
 		  driver.close();
 		  driver.quit();
+		  }catch(Exception e){
+			  System.out.println("FF issue");
+		  }
 	  }
 
 }
